8f6bc8a9dcffbe0c10915444ce9ea6761b011ed7
Tejasri2716 <tejashrees1610@gmail.com>
2024-09-24 10:50:04 +0530

    Update userRoutes.js

--- ex3/userRoutes.js
+++ ex3/userRoutes.js
@@ -1,26 +1,43 @@
  // routes/userRoutes.js
  const express = require('express');
- const User = require('../models/User');
- const Post = require('../models/Post');
- const router = express.Router();
+const User = require('../models/User');
+const Post = require('../models/Post');
+const router = express.Router();
 
- router.post('/', async (req, res) => {
-     const user = new User(req.body);
-     await user.save();
-     res.send(user);
- });
+router.post('/', async (req, res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.send(user);
+});
 
- router.post('/:id/follow', async (req, res) => {
-     const user = await User.findById(req.params.id);
-     user.followers.push(req.body.followerId);
-     await user.save();
-     res.send(user);
- });
+router.post('/:id/follow', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    user.followers.push(req.body.followerId);
+    await user.save();
+    res.send(user);
+});
 
- router.get('/:id/posts', async (req, res) => {
-     const user = await User.findById(req.params.id).populate('followers');
-     const posts = await Post.find({ author: { $in: user.followers } }).populate('author');
-     res.send(posts);
- });
+// routes/userRoutes.js
+router.get('/:id/posts', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).populate('followers');
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        
+        // Check if user has followers
+        if (!user.followers || user.followers.length === 0) {
+            return res.status(404).send('No followers found for this user');
+        }
 
- module.exports = router;
+        // Find posts from followers
+        const posts = await Post.find({ author: { $in: user.followers } }).populate('author');
+        res.send(posts);
+    } catch (err) {
+        console.error('Error fetching posts:', err);
+        res.status(500).send('Server error');
+    }
+});
+
+
+module.exports = router;
