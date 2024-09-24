f905b12b3dc0af0cb801fe465adf4cc2fa934aa6
Tejasri2716 <tejashrees1610@gmail.com>
2024-09-24 10:48:30 +0530

    Update index.js

--- ex3/index.js
+++ ex3/index.js
@@ -1,21 +1,20 @@
  // index.js
- const express = require('express');
- const mongoose = require('./config/db');
- const bodyParser = require('body-parser');
- const cors = require('cors');
+  const express = require('express');
+const mongoose = require('./config/db');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
- const app = express();
- app.use(bodyParser.json());
- app.use(cors());
- app.use(express.static('public'));
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('public'));
 
- const userRoutes = require('./routes/userRoutes');
- const postRoutes = require('./routes/postRoutes');
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
 
- app.use('/users', userRoutes);
- app.use('/posts', postRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
- app.listen(3000, () => {
-     console.log('Server is running on http://localhost:3000');
- });
- 
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
