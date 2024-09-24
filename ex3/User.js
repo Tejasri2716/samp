d75ec418d9aa9260f65890192e042b47d56463af
Tejasri2716 <tejashrees1610@gmail.com>
2024-09-24 10:43:50 +0530

    Update User.js

--- ex3/User.js
+++ ex3/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: String,
-    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]  // Array of follower IDs
 });
 
 module.exports = mongoose.model('User', userSchema);
